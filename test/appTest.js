056a4d2838d1c5a013ac0deec33a1a6dfff63f44
jatinkmr <kumar.jatin873@gmail.com>
2019-04-20 13:49:18 +0530

    Other Function Added

--- test/appTest.js
+++ test/appTest.js
@@ -1,5 +1,6 @@
 const assert = require('chai').assert;
 const sayHello = require('../app').sayHello;
+const addNumbers = require('../app').addNumbers;
 // const app = require('../app');
 
 describe('App', () => { 
@@ -11,5 +12,15 @@ describe('App', () => {
 	it('sayHello should return type String', () => { 
 		let result = sayHello();
 		assert.typeOf(result, 'string');
+	});
+
+	it('addNumbers should be above 5', () => { 
+		let result = addNumbers(5, 5);
+		assert.isAbove(result, 5);
+	});
+
+	it('addNumbers should return type Number', () => { 
+		let result = addNumbers(5, 5);
+		assert.typeOf(result, 'number');
 	});
 });
