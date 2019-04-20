30c68afa4cfef5aa647ded7a703cbb1d3ea77884
jatinkmr <kumar.jatin873@gmail.com>
2019-04-20 13:55:30 +0530

    Function Group Added

--- test/appTest.js
+++ test/appTest.js
@@ -7,23 +7,27 @@ sayHelloResult = app.sayHello();
 addNumbersResult = app.addNumbers(5, 6);
 
 describe('App', () => { 
-	it('sayHello Should return hello', () => { 
-		// let result = app.sayHello();
-		assert.equal(sayHelloResult, 'hello');
+	describe('sayHello()', () => { 
+		it('sayHello Should return hello', () => { 
+			// let result = app.sayHello();
+			assert.equal(sayHelloResult, 'hello');
+		});
+	
+		it('sayHello should return type String', () => { 
+			// let result = app.sayHello();
+			assert.typeOf(sayHelloResult, 'string');
+		});
 	});
 
-	it('sayHello should return type String', () => { 
-		// let result = app.sayHello();
-		assert.typeOf(sayHelloResult, 'string');
-	});
-
-	it('addNumbers should be above 5', () => { 
-		// let result = app.addNumbers(5, 5);
-		assert.isAbove(addNumbersResult, 5);
-	});
-
-	it('addNumbers should return type Number', () => { 
-		// let result = app.addNumbers(5, 5);
-		assert.typeOf(addNumbersResult, 'number');
+	describe('addNumbers', () => { 
+		it('addNumbers should be above 5', () => { 
+			// let result = app.addNumbers(5, 5);
+			assert.isAbove(addNumbersResult, 5);
+		});
+	
+		it('addNumbers should return type Number', () => { 
+			// let result = app.addNumbers(5, 5);
+			assert.typeOf(addNumbersResult, 'number');
+		});
 	});
 });
