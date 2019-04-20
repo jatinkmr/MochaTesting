c79f7cf441e91b256807a53904dc31c6565424bb
jatinkmr <kumar.jatin873@gmail.com>
2019-04-20 13:53:01 +0530

    Global Variable Added

--- test/appTest.js
+++ test/appTest.js
@@ -1,26 +1,29 @@
 const assert = require('chai').assert;
-const sayHello = require('../app').sayHello;
-const addNumbers = require('../app').addNumbers;
-// const app = require('../app');
+// const sayHello = require('../app').sayHello;
+// const addNumbers = require('../app').addNumbers;
+const app = require('../app');
+
+sayHelloResult = app.sayHello();
+addNumbersResult = app.addNumbers(5, 6);
 
 describe('App', () => { 
 	it('sayHello Should return hello', () => { 
-		let result = sayHello();
-		assert.equal(result, 'hello');
+		// let result = app.sayHello();
+		assert.equal(sayHelloResult, 'hello');
 	});
 
 	it('sayHello should return type String', () => { 
-		let result = sayHello();
-		assert.typeOf(result, 'string');
+		// let result = app.sayHello();
+		assert.typeOf(sayHelloResult, 'string');
 	});
 
 	it('addNumbers should be above 5', () => { 
-		let result = addNumbers(5, 5);
-		assert.isAbove(result, 5);
+		// let result = app.addNumbers(5, 5);
+		assert.isAbove(addNumbersResult, 5);
 	});
 
 	it('addNumbers should return type Number', () => { 
-		let result = addNumbers(5, 5);
-		assert.typeOf(result, 'number');
+		// let result = app.addNumbers(5, 5);
+		assert.typeOf(addNumbersResult, 'number');
 	});
 });
